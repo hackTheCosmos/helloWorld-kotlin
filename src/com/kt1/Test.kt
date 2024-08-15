e154b6a6dd84e77ef10a506d3449238873a3f674
HackTheCosmos44 <baileche.theo@gmail.com>
2024-08-15 16:40:37 +0200

    variables

--- src/com/kt1/Test.kt
+++ src/com/kt1/Test.kt
@@ -1,6 +1,27 @@
 package com.kt1
 
+//constante (const val) :: must be assigned a value at compile time, can't be local
+const val DATE_OF_BIRTH: Int = 1989
+
 fun main () {
 	
-	println("hello world")
+	//constante (val) : can be assigned a value at runtime, must be local
+	val name = "jo"
+	
+	
+	//variable (var) avec inférence de type(détection automatique du type de la variable, c'est optionnel)
+	var greetings:String = "hello"
+	
+	//variable qui peut être possiblement nulle(?)
+	var religion:String? = "xyz"
+	//on vérifie si la variable "religion" est nulle avant de la modifier
+//	religion?.uppercase()
+	
+	//affiche variables en console
+	println(name)
+	println("Hello $name")
+	println (greetings)
+	println (religion)
+	println(DATE_OF_BIRTH)
+	
 }
